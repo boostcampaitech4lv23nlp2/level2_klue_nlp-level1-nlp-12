ced5a1ee016be3f7147153b238c7994b015aaf42
정석희 <86893209+seokhee516@users.noreply.github.com>
2023-02-10 20:17:55 +0900

    chore: remove wandb token

--- pl/main_n.py
+++ pl/main_n.py
@@ -18,11 +18,7 @@ time_ = datetime.now() + timedelta(hours=9)
 time_now = time_.strftime("%m%d%H%M")
 
 wandb_dict = {
-    "gwkim_22": "f631be718175f02da4e2f651225fadb8541b3cd9",
-    "rion_": "0d57da7f9222522c1a3dbb645a622000e0344d36",
-    "daniel0801": "b8c4d92272716adcb1b2df6597cfba448854ff90",
-    "seokhee": "c79d118b300d6cff52a644b8ae6ab0933723a59f",
-    "dk100": "263b9353ecef00e35bdf063a51a82183544958cc",
+    "user": "token"
 }
 
 if __name__ == "__main__":
